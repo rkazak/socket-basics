01ef0b88e30b292be60f41eddba5654ebd168543
Rohinton Kazak <rohinton.kazak@gmail.com>
2015-12-23 02:55:35 -0800

    Add disconnect notification and event

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ var clientInfo = {};
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io');
 
+	socket.on('disconnect', function () {
+		var userData = clientInfo[socket.id];
+
+		if (typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left!',
+				timestamp: moment.valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
@@ -27,6 +41,7 @@ io.on('connection', function (socket) {
 
 		message.ts = moment().valueOf();
 		io.to(clientInfo[socket.id].room).emit('message', message);
+
 	});
 
 	socket.emit('message', {
